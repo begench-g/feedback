009338a46dc0cb0e482e930000bd32af5bededd1
unknown <forgithubtobegench@gmail.com>
2022-11-22 22:16:00 +0500

    'initial'

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import {useState} from 'react';
+import FeedbackData from "./data/FeedbackData";
+import FeedbackItem from "./components/FeedbackItem";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
+import {FeedbackProvider} from "./context/FeedbackContext";
+
+
+function App(){
+    const [feedback,setFeedback]=useState(FeedbackData)
+
+
+    return (
+        <FeedbackProvider>
+        <Header text='Hello world'/>
+        <div className="container">
+            <FeedbackForm />
+            <FeedbackStats />
+            <FeedbackList />
+        </div>
+        </FeedbackProvider>
+    )
+}
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+const Header = (props) => {
+  return (
+    <header>
+        <div className="container">
+            <h2>{props.text}</h2>
+        </div>
+    </header>
+  )
+}
+Header.defaultProps={
+    text:"Feedback UI",
+}
+Header.propTypes={
+    text:PropTypes.string,
+}
+
+export default Header;
